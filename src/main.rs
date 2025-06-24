b1e35443d69213f72b6772700f033b6cc471cb32
Rajan Choksi <rachoksi9999@gmail.com>
2025-06-24 13:53:07 +0530

    third commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use std::fs;
 use std::env;
+use std::fs;
 use std::process;
 
 // this is for creating directory or directories
 fn create_directory(path: &str) {
     match fs::create_dir(path) {
-        Ok(_) => println!("created directory: {}", path),
-        Err(e) => eprintln!("Error occured while creating directory, {}", e),
+        Ok(_) => println!("\ncreated directory: {}\n", path),
+        Err(e) => eprintln!("\nError occured while creating directory, {}\n", e),
     };
 }
 
-// fn remove_direcotory() {
-
-// }
+fn remove_direcotory(path: &str) {
+    match fs::remove_dir(path) {
+        Ok(_) => println!("\nremoved directory: {}\n", path),
+        Err(e) => eprintln!("\nError occured while removing directory, {}\n", e),
+    };
+}
 
 // fn move_directory() {
 
@@ -24,10 +27,13 @@ fn create_directory(path: &str) {
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    
+
+    // generate error and end program, if there are less then 3 args, because either path or flag or both not provided.
     if arguments.len() < 3 {
-        eprintln!("option or path or both are missing, please provide valid option and path to create directory");
-        process::exit(1);        
+        eprintln!(
+            "\noption or path or both are missing, please provide valid option and path to create directory\n"
+        );
+        process::exit(1);
     }
 
     // for now i will using flag temporarily will make it paramenent
@@ -36,9 +42,12 @@ fn main() {
 
     if flag == "-c" {
         create_directory(&address);
-    }
-    else {
-        println!("wrong option is provided, provide valid option");
+    } else if flag == "-r" {
+        remove_direcotory(&address);
+    } else {
+        println!("\nWrong option is provided, provide valid option\n");
         println!("'-- -c' for creating directory");
+        println!("'-- -r' for removing directory");
+        println!();
     }
 }
