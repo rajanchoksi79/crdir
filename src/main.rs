5822def7891b5c454e0d904b83a5506a22ba4528
Rajan Choksi <rachoksi9999@gmail.com>
2025-06-25 11:25:25 +0530

    fourth commit

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use std::process;
 // this is for creating directory or directories
 fn create_directory(path: &str) {
     match fs::create_dir(path) {
-        Ok(_) => println!("\ncreated directory: {}\n", path),
+        Ok(_) => println!("\nCreated directory: {}\n", path),
         Err(e) => eprintln!("\nError occured while creating directory, {}\n", e),
     };
 }
 
 fn remove_direcotory(path: &str) {
     match fs::remove_dir(path) {
-        Ok(_) => println!("\nremoved directory: {}\n", path),
+        Ok(_) => println!("\nRemoved directory: {}\n", path),
         Err(e) => eprintln!("\nError occured while removing directory, {}\n", e),
     };
 }
@@ -25,11 +25,21 @@ fn remove_direcotory(path: &str) {
 
 // }
 
+fn know_current_directory() {
+    match env::current_dir() {
+        Ok(p) => println!("\nCurrent working directory is: {}\n", p.display()),
+        Err(e) => eprintln!(
+            "\nError occured while providing current working directory {}\n",
+            e
+        ),
+    };
+}
+
 fn main() {
     let arguments: Vec<String> = env::args().collect();
 
     // generate error and end program, if there are less then 3 args, because either path or flag or both not provided.
-    if arguments.len() < 3 {
+    if arguments.len() < 3 && arguments[1] != "-p" {
         eprintln!(
             "\noption or path or both are missing, please provide valid option and path to create directory\n"
         );
@@ -44,6 +54,8 @@ fn main() {
         create_directory(&address);
     } else if flag == "-r" {
         remove_direcotory(&address);
+    } else if flag == "-p" {
+        know_current_directory();
     } else {
         println!("\nWrong option is provided, provide valid option\n");
         println!("'-- -c' for creating directory");
